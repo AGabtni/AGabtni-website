b83b705af53b79d54c237d7e710e7dd5b9e66528
AGabtni <agabt077@gmail.com>
2019-11-02 01:19:25 -0400

    fixed animations and overlay on mobile

--- src/app/top-navbar/top-navbar.component.ts
+++ src/app/top-navbar/top-navbar.component.ts
@@ -1,6 +1,16 @@
-import { Component,Input, OnInit, ViewEncapsulation,ViewChild, HostListener, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation,ViewChild, ElementRef,Inject } from '@angular/core';
 import { categories } from '../static/categories';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+
+import { DOCUMENT } from '@angular/common';
 
 
 @Component({
@@ -8,41 +18,68 @@ import { categories } from '../static/categories';
   templateUrl: './top-navbar.component.html',
   styleUrls: ['./top-navbar.component.css'],
   encapsulation: ViewEncapsulation.None,
+  animations : [
+
+      trigger('messageAnimation', [
+
+      state('visible', 
+      style({  
+          opacity: 1.0,
+          transform: 'translateX(0%)',
+          }),
+
+
+      ),
+
+      state('hidden',   style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+      })),
+
+      transition('hidden => visible', animate('800ms 300ms ease')),
+      transition('visible => hidden', animate('200ms ease-out'))
+
+    ]),
+    
+
+  ]
  
 })
 export class TopNavbarComponent implements OnInit {
   
 
   @ViewChild('MobielOverlay' , {static: true}) overlay : ElementRef;
+  @ViewChild('body' , {static: true}) body : ElementRef;
   
 
   title = 'Website Title';
-
+  hidden = true;
 
   categories = categories ;
 
   openNav(){ 
-
+    this.document.body.style.overflow = "hidden";
     this.overlay.nativeElement.style.width = "100%"
+    this.hidden = false;
   }
 
   closeNav(){ 
+    this.hidden = true;
+    this.document.body.style.overflow = "unset";
 
     this.overlay.nativeElement.style.width = "0%"
   }
 
 
-  @Input()
-  hasBackdrop: false;
 
-  constructor() { 
+  constructor(@Inject(DOCUMENT) private document: any) { 
 
 
   }
 
   ngOnInit() {
 
-    console.log(this.overlay);
+    console.log(this.body);
   }
 
 }
