393987f4752a3cb3a2d8f02b49eb22b29f71894e
Ahmed <Ahmed@VNR1020-001.uottawa.o.univ>
2019-11-13 12:50:08 -0500

    Rectifying layouts and adding animations

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit,OnDestroy, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import {cardHover} from '../../assets/animations';
 
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
+  animations: [cardHover]
 })
 export class ServicesComponent implements OnInit, OnDestroy {
  
+
+  states: Array<boolean> = [false,false,false,false,false,false];
+
   constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() {
+  	
   }
+
 
   ngOnDestroy(){
   }
+
 
 }
